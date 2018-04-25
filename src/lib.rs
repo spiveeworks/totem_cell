67ec83081826d81df74e514da1710d835db4f5cf
SpiveeWorks <spiveehere@gmail.com>
2018-04-25 16:08:55 +1000

    is that it

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::cell;
+
+
+pub struct Totem {
+    _private: ()
+}
+
+impl Totem {
+    pub unsafe fn new() -> Self {
+        Totem {
+            _private: ()
+        }
     }
 }
+
+pub struct TotemCell<T> {
+    pub inner: cell::UnsafeCell<T>
+}
+
+impl<T> TotemCell<T> {
+    pub fn new(val: T) -> Self {
+        let inner = cell::UnsafeCell::new(val);
+        TotemCell { inner }
+    }
+
+    pub fn into_inner(self: Self) -> T {
+        self.inner.into_inner()
+    }
+
+    pub fn borrow(self: &Self, _totem: &Totem) -> &T {
+        unsafe {
+            &*self.inner.get()
+        }
+    }
+
+    pub fn borrow_mut(self: &Self, _totem: &mut Totem) -> &mut T {
+        unsafe {
+            &mut *self.inner.get()
+        }
+    }
+}
+
